8b525dd8871af3ef76f487311fb80ac2552f2acc
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-10 14:51:24 -0400

    post component and next config for images and blog tags

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io'], // Add the Sanity CDN domain here
+    },
+  };
+  
+  export default nextConfig; // Use export default for ES modules
+  
