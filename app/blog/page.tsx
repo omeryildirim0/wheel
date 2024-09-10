8501bcb50462186287bde73e5012cef913ca06af
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-10 09:54:27 -0400

    get posts for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import { client } from "@/sanity/lib/client";
 
+
+async function getPosts() {
+  const query = `
+  *[_type == "post"] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    tags[]-> {
+      _id,
+      slug,
+      name
+    }
+  }
+  `;
+  const data = await client.fetch(query);
+  return data;
+}
+
+
 const blog = () => {
   return (
     <div>blog</div>
