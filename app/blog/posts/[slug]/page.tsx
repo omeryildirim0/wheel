0212b560d6efc9a7e6eb220ac92b0f825efe9c67
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-11 10:12:33 -0400

    routing issue resolved

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -103,7 +103,7 @@ const page = async ({ params, searchParams }: Params) => {
         </span>
         <div className="mt-5">
           {post?.tags?.map((tag) => (
-            <Link key={tag?._id} href={`/tag/${tag.slug.current}`}>
+            <Link key={tag?._id} href={`/blog/tag/${tag.slug.current}`}>
               <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">
                 #{tag.name}
               </span>
