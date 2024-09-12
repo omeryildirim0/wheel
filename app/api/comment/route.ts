5b420e90a91094188c44161bb22de6b68a85bd79
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-12 14:09:35 -0400

    comment functionality is working

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -1,7 +1,7 @@
 import { client } from "@/sanity/lib/client";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   const data = await req.json();
   const { name, email, comment, postId } = data;
 
@@ -26,12 +26,16 @@ export async function POST(req: Request, res: Response) {
       },
     });
     return NextResponse.json(
-      { message: "Comment added successfully", commet: newComment },
+      { message: "Comment added successfully", comment: newComment },
       { status: 201 }
     );
   } catch (error) {
+    // Use a type assertion to ensure 'error' is treated as an instance of Error
+    const errorMessage = (error as Error).message || 'Unknown error';
+    console.error("Error creating comment:", errorMessage);
+
     return NextResponse.json(
-      { message: "Failed to create a comment", error },
+      { message: "Failed to create a comment", error: errorMessage },
       { status: 500 }
     );
   }
