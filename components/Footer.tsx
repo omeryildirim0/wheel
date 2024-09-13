28a58308af99bf565985bc8148f5f95549cfa4ee
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-13 10:28:04 -0400

    simler footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,67 +3,37 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        {/* Left Section: Brand and Description */}
-        <div className="mb-4 md:mb-0 text-center md:text-left">
-          <h1 className="text-2xl font-bold">Wheel of Meals</h1>
-          <p className="text-sm mt-2">
-            Discover new dining experiences with a spin of the wheel!
-          </p>
-        </div>
-
-        {/* Center Section: Links */}
-        <div className="flex justify-center md:justify-start mb-4 md:mb-0">
-          <ul className="flex space-x-4">
+    <footer className="bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white py-4">
+      <div className="container mx-auto text-center">
+        {/* Navigation Links */}
+        <div className="mb-2">
+          <ul className="inline-flex space-x-6">
             <li>
-              <a href="/about" className="text-sm hover:text-yellow-500">
-                About Us
+              <a href="/" className="hover:underline">
+                Home
               </a>
             </li>
             <li>
-              <a href="/blog" className="text-sm hover:text-yellow-500">
+              <a href="/about" className="hover:underline">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/blog" className="hover:underline">
                 Blog
               </a>
             </li>
             <li>
-              <a href="/contact" className="text-sm hover:text-yellow-500">
+              <a href="/contact" className="hover:underline">
                 Contact
               </a>
             </li>
           </ul>
         </div>
 
-        {/* Right Section: Social Media Icons */}
-        <div className="flex justify-center md:justify-end space-x-6">
-          <a
-            href="https://twitter.com"
-            className="text-xl hover:text-yellow-500"
-            aria-label="Twitter"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a
-            href="https://instagram.com"
-            className="text-xl hover:text-yellow-500"
-            aria-label="Instagram"
-          >
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a
-            href="https://facebook.com"
-            className="text-xl hover:text-yellow-500"
-            aria-label="Facebook"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
-        </div>
-      </div>
-
-      {/* Bottom Section: Copyright */}
-      <div className="text-center mt-6">
-        <p className="text-xs">
-          © {new Date().getFullYear()} Wheel of Meals. All Rights Reserved.
+        {/* Copyright Text */}
+        <p className="text-sm mt-2">
+          © {new Date().getFullYear()} Wheel of Meals. All rights reserved.
         </p>
       </div>
     </footer>
