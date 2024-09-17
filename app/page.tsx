7c9a625a65aed127ba724fe17d00aed000551b37
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-16 20:12:50 -0400

    header main

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Wheel from '@/components/Wheel/Wheel';
 import SearchBar from '@/components/SearchBar';
 import useSearch from '@/hooks/useSearch';
+import HeaderMain from '@/components/HeaderMain';
 
 
 const Home = () => {
@@ -17,6 +18,7 @@ const Home = () => {
   return (
     <div className="max-w-full mx-auto p-0">
       <div className="max-w-lg mx-auto text-center mt-20">
+
         <h1 className="text-2xl font-bold mt-5 mb-6">Find Restaurants</h1>
         <SearchBar onSearch={onSearch} />
 
