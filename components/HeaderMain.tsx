97290c56c6f7966cdb2dd76fca8efb71a8234cb2
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-16 20:48:33 -0400

    dark mode

--- components/HeaderMain.tsx
+++ components/HeaderMain.tsx
@@ -1,9 +1,9 @@
 const HeaderMain = () => {
     return (
-      <div className="bg-gradient-to-r from-purple-200 via-pink-200 to-red-200 text-gray-800 text-center py-12 px-6 shadow-lg w-full">
+      <div className="bg-gradient-to-r from-purple-300 via-pink-300 to-red-300 dark:from-gray-900 dark:via-gray-800 dark:to-black text-gray-900 dark:text-white text-center py-14 px-6 shadow-lg w-full">
         <h1 className="text-4xl font-extrabold mb-2">Welcome to Wheel of Meals!</h1>
         <p className="text-xl font-light">
-          Can't decide where to eat? Let us help! Enter your zipcode, spin the wheel, discover new restaurants, and enjoy a delightful meal near you. It's fun, easy, and delicious!
+          Can't decide where to eat? Let us help! Spin the wheel, discover new restaurants, and enjoy a delightful meal near you. It's fun, easy, and delicious!
         </p>
       </div>
     );
