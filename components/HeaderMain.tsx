6fd6a5a4913dc25d0e6aaf45c75a18a32f90c92d
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-16 20:37:18 -0400

    changes to header main

--- components/HeaderMain.tsx
+++ components/HeaderMain.tsx
@@ -1,13 +1,13 @@
 const HeaderMain = () => {
     return (
-      <div className="bg-gradient-to-r from-purple-200 via-pink-200 to-red-200 text-gray-800 text-center py-8 px-6 shadow-lg w-full">
+      <div className="bg-gradient-to-r from-purple-200 via-pink-200 to-red-200 text-gray-800 text-center py-12 px-6 shadow-lg w-full">
         <h1 className="text-4xl font-extrabold mb-2">Welcome to Wheel of Meals!</h1>
         <p className="text-xl font-light">
-          Can't decide where to eat? Let us help! Spin the wheel, discover new restaurants, and enjoy a delightful meal near you. It's fun, easy, and delicious!
+          Can't decide where to eat? Let us help! Enter your zipcode, spin the wheel, discover new restaurants, and enjoy a delightful meal near you. It's fun, easy, and delicious!
         </p>
       </div>
     );
-  };
+};
   
-  export default HeaderMain;
+export default HeaderMain;
   
