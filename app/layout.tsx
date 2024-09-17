2106b8000ad90ca2554f893093947d6f5b92bfa9
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-17 11:09:51 -0400

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,43 @@ import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Wheel of Meals",
-  description: "A fun way to decide where to eat",
+// root layout.tsx
+
+export const metadata = {
+  title: 'Wheel of Meals - Find Your Next Meal Adventure',
+  description: 'Spin the wheel to discover amazing restaurant options near you with the Wheel of Meals app. Perfect for those who can’t decide where to eat!',
+  keywords: 'restaurant, food, meal, dining, eat out, restaurant finder, food wheel, Wheel of Meals, lunch, dinner, next meal, random restaurant picker, explore cuisine',
+  author: 'Omer Yildirim',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.wheelofmeals.com',
+    title: 'Wheel of Meals - Find Your Next Meal Adventure',
+    description: 'Can’t decide where to eat? Let the Wheel of Meals pick a restaurant for you. Discover and explore new dining experiences near you!',
+    siteName: 'Wheel of Meals',
+    images: [
+      {
+        url: 'https://www.wheelofmeals.com/images/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Wheel of Meals Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@wheelofmeals',
+    creator: '@omeryildirim',
+    title: 'Wheel of Meals - Find Your Next Meal Adventure',
+    description: 'Let the Wheel of Meals choose your next restaurant! Spin the wheel and explore great dining options nearby.',
+    images: ['https://www.wheelofmeals.com/images/og-image.jpg'],
+  },
+  icons: {
+    icon: '/favicon.ico',
+    appleTouchIcon: '/apple-touch-icon.png',
+  },
 };
+
 
 export default function RootLayout({
   children,
