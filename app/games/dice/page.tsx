abb364c29f3766733a4aceb153b84d478af76595
omeryildirim0 <152308714+omeryildirim0@users.noreply.github.com>
2024-09-26 21:10:13 -0400

    margins

--- app/games/dice/page.tsx
+++ app/games/dice/page.tsx
@@ -70,7 +70,7 @@ const LuckOfTheDice = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center px-4 sm:px-8 md:px-12 lg:px-24">
-      <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center">Luck of the Dice</h1>
+      <h1 className="text-3xl md:text-4xl font-bold mb-6 mt-20 text-center">Luck of the Dice</h1>
 
       <div className="mb-4 w-full sm:w-auto">
         <label className="mr-2 text-lg">Number of Players:</label>
